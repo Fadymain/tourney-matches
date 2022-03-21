bc8c9042c5d070575e593474480c6b4b40ee455d
Adetola <adetolafadairo@gmail.com>
2022-03-21 11:10:18 -0700

    passing data down as props

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,15 +1,16 @@
 import Player from "./Player";
-import matchData from '../data/matchData';
-import playerData from '../data/playerData';
+import matchData from '../data/MatchData';
+import playerData from '../data/PlayerData';
 import {preparePlayerData, addWinsToPlayers} from "../helpers/playerHelpers"
 function PlayerList(props) {
   const playerDataArray = preparePlayerData(playerData);
   const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  const onePlayer = parsedPlayerData[0];
 
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
-      <Player />
+      <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
     </section>
   );
 }
